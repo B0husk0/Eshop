3f1469084dff8801eac916d4c7c15a2b63a8f8b8
Matúš Bohucký <bohus98@gmail.com>
2025-09-28 16:15:43 +0200

    add controller and DTO

---
+++ Eshop.Api/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Eshop.Api.Data;
+using Eshop.Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshop.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Returns all products.
+        /// </summary>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
+        {
+            var products = await _context.Products
+                .Include(p => p.Category)
+                .Select(p => new ProductDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    ImageUrl = p.ImageUrl,
+                    Price = p.Price,
+                    Description = p.Description,
+                    Quantity = p.Quantity,
+                    Category = p.Category != null ? p.Category.Name : string.Empty
+                })
+                .ToListAsync();
+
+            return Ok(products);
+        }
+    }
+}
